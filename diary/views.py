cbd8316487fa8ba043834388c82926767f67d49a
Banovšek Veno Lan <banovsekv@ts.telekom.si>
2021-04-01 03:25:09 +0100

    Added URL configuration for diary and user apps' main landing pages

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('This is the diary landing page. It loads the data \
+      for a specific diary and displays it (so, the entries).')
