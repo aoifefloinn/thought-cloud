dfe3bf1f4ba0167e7d77ff96b5ceedb0cf53a6c7
Banovšek Veno Lan <banovsekv@ts.telekom.si>
2021-04-02 15:35:32 +0100

    Connected the user dashboard page and diary page together, with basic placeholder content

--- user/views.py
+++ user/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse('This is the user dashboard. It will display \
-      user info and diaries written (or it will redirect to the login page).')
+    template = loader.get_template('user/dashboard.html')
+    context = {
+        'page_description': 'This is the user dashboard. It will display \
+          user info and diaries written (or it will redirect to the login page).',
+    }
+    return HttpResponse(template.render(context, request))
