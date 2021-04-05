f96546ee9836517df3562e87afb50da0cf27acf6
Hyeseon Lee <hyesun1999@gmail.com>
2021-04-06 00:12:55 +0900

    change auth/ url name

--- thought_cloud/urls.py
+++ thought_cloud/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('diary.urls')),
-    path('auth/', include('user.urls')),
+    path('diary/', include('diary.urls')),
+    path('', include('user.urls')),
 ]
